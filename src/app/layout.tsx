631bc356d6c06972d56824fe43eb2b612bb87456
Ibrahim Bakari <ibrahimwali4u@gmail.com>
2025-05-16 10:52:46 +0000

    createUnhandledError@https://6000-firebase-studio-1746617140202.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:879:71 handleClientError@https://6000-firebase-studio-1746617140202.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:1052:56 error@https://6000-firebase-studio-1746617140202.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:1191:56 getRootForUpdatedFiber@https://6000-firebase-studio-1746617140202.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:4702:143 enqueueConcurrentHookUpdate@https://6000-firebase-studio-1746617140202.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:4685:16 dispatchSetStateInternal@https://6000-firebase-studio-1746617140202.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:6448:22 dispatchSetState@https://6000-firebase-studio-1746617140202.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:6421:33 [project]/node_modules/next/dist/client/components/react-dev-overlay/ui/components/errors/dev-tools-indicator/next-logo.js [app-client] (ecmascript)/useMeasureWidth/</observer<@https://6000-firebase-studio-1746617140202.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:11835:21

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import './globals.css';
@@ -18,7 +19,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body 
         className={`${GeistSans.className} antialiased`} // Use .className for GeistSans
-        suppressHydrationWarning // Add to body as well for extension issues
+        // suppressHydrationWarning was removed from here as it's already on <html>
       >
         <AuthProvider>
           {children}
